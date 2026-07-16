cef32817780563815c506a6a0aafef92c7ccef23
ItxUzair1 <muhammaduzair1411@gamil.com>
2026-07-16 19:16:11 +0500

    Implement Calculator

---
+++ test/test-operation.py
@@ -0,0 +1,15 @@
+from src.calculator import add,sub
+
+def test_add():
+    assert add(1,3)==4
+    assert add(3,4)==7
+    assert add(-1,-1)==-2
+    assert add(0.-3)==-3
+    assert add(0,0)==0
+    
+    
+def test_sub():
+    assert sub(0,1)==-1
+    assert sub(6,3)==3
+    assert sub(4,5)==-1
+    assert sub(0,0)==0
